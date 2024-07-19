a3dd9b8469a98f7b85af348d951e7b3f29ff3c92
zenpoxa <zenpoxa@gmail.com>
2024-07-19 16:00:13 +0200

    projet terminé (avec ajout nom en paramètre de l'éxécutable)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use std::error::Error;
+use std::{error::Error};
 
 use csv;
 
 fn read_from_file(path: &str) -> Result<(), Box<dyn Error>>{
     let mut reader = csv::Reader::from_path(path)?;
 
-    for result in reader.records(){
+    for result in reader.records() {
         let record = result?;
         println!("{:?}", record);
     }
+
     Ok(())
 }
 
 fn main() {
-    if let Err(e) = read_from_file("./customers.csv"){
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() < 2 {
+        panic!("!!! Vous devez rentrer un nom de fichier csv !!!");
+    }
+
+    // c'est bon, un fichier csv est passé en paramètre
+    let fname = std::path::Path::new(&*args[1]);
+
+    if let Err(e) = read_from_file(&fname.display().to_string()){
         eprintln!("{}", e);
     }
 }
